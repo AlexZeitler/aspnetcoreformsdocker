a43164d3a9c5a40241e197604b9818298deae08f
Unknown <alexander.zeitler@pdmlab.com>
2017-05-12 23:47:02 +0200

    add json post endpoint

---
+++ AspNetCoreFormsDockerNginx/Controllers/ValuesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreFormsDockerNginx.Controllers
+{
+    [Route("api/values")]
+    public class ValuesController : Controller
+    {
+        public ActionResult Post([FromBody]string[] test)
+        {
+             return new JsonResult(test);
+        }
+    }
+}
